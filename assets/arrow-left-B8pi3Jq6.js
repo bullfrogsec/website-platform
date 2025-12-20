505b6d0c4791aae4ae6ca7b322c6fc6b772ce1c9
fallard84 <fallard84@users.noreply.github.com>
2025-12-20 03:41:52 +0000

    deploy: bullfrogsec/website-platform-source@d112882640095b1d1f6602bcd5fee51bf126d176

--- assets/arrow-left-B8pi3Jq6.js
+++ assets/arrow-left-B8pi3Jq6.js
@@ -1,4 +1,4 @@
-import{c as o}from"./Footer-Ds8e-x78.js";/**
+import{c as o}from"./Footer-D5ZjfAh2.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
